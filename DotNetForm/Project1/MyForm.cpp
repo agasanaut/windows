10fcbf6a1c69e7e7108bdcd51bb2a67e04dbab03
tejus <agasanaut@gmail.com>
2022-11-21 01:13:07 +0530

    new form

---
+++ DotNetForm/Project1/MyForm.cpp
@@ -0,0 +1,20 @@
+#include "MyForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+using namespace Project1;
+//go to MyForm.h and look for [namespace ****] where **** is the name of your project..mines name is testing. :)
+
+
+STAThreadAttribute;
+
+[STAThread]
+void main() {
+    
+    std::cout << "Start....";
+
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+    Application::Run(gcnew MyForm());
+}
+
