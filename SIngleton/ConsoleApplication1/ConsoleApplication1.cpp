fdce2ffa4cd306a6e21f5504911c8aed100c854b
Tejus s <agasanaut@gmail.com>
2023-08-07 09:31:26 +0530

    .

--- SIngleton/ConsoleApplication1/ConsoleApplication1.cpp
+++ SIngleton/ConsoleApplication1/ConsoleApplication1.cpp
@@ -6,24 +6,22 @@ using namespace std;
 class Singleton
 {
 private:
-    static Singleton* obj;
-    Singleton() {}
-    static mutex m;
-
-
+	static Singleton* obj;
+	Singleton() {}
+	static mutex m;
 public:
-    static Singleton* getInstance()
-    {
-        if (obj == nullptr)
-        {
-            lock_guard<mutex>lg(m);
-            if (obj == nullptr)
-            {
-                obj = new Singleton();
-            }
-        }
-        return obj;
-    }
+	static Singleton* getInstance()
+	{
+		if (obj == nullptr)
+		{
+			lock_guard<mutex>lg(m);
+			if (obj == nullptr)
+			{
+				obj = new Singleton();
+			}
+		}
+		return obj;
+	}
 };
 
 Singleton* Singleton::obj = nullptr;
@@ -31,9 +29,9 @@ mutex Singleton::m;
 
 int main()
 {
-    auto obj1 = Singleton::getInstance();
-    auto obj2 = Singleton::getInstance();
-
-    std::cout << "Singleton obj created - !!!\n"<< "  "<<obj1<<" , "<<obj2;
+	auto obj1 = Singleton::getInstance();
+	auto obj2 = Singleton::getInstance();
+	std::cout << "Singleton obj created - !!!\n" << "  " << obj1 << " , " << obj2;
+	return EXIT_SUCCESS;
 }
 
