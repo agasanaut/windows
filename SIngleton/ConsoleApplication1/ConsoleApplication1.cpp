10f33b22cd4a8fed0a21477455409ed6c7155c5e
Tejus s <agasanaut@gmail.com>
2023-08-07 09:30:07 +0530

    thread safe impl

--- SIngleton/ConsoleApplication1/ConsoleApplication1.cpp
+++ SIngleton/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,26 +1,39 @@
 #include <iostream>
+#include <mutex>
+
+using namespace std;
 
 class Singleton
 {
 private:
     static Singleton* obj;
     Singleton() {}
+    static mutex m;
+
+
 public:
     static Singleton* getInstance()
     {
         if (obj == nullptr)
         {
-            obj = new Singleton();
+            lock_guard<mutex>lg(m);
+            if (obj == nullptr)
+            {
+                obj = new Singleton();
+            }
         }
         return obj;
     }
 };
 
 Singleton* Singleton::obj = nullptr;
+mutex Singleton::m;
 
 int main()
 {
-    Singleton::getInstance();
-    std::cout << "Singleton obj created - !!!\n";
+    auto obj1 = Singleton::getInstance();
+    auto obj2 = Singleton::getInstance();
+
+    std::cout << "Singleton obj created - !!!\n"<< "  "<<obj1<<" , "<<obj2;
 }
 
